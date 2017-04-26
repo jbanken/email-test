d0d101008bfa9a53fca498aeee17df54ba8d0c86
JBANKENHOMELAP\joe <jbanken@gmail.com>
2017-04-25 20:41:01 -0500

    Added swagger.

--- Email/Startup.cs
+++ Email/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Email
 {
@@ -28,6 +29,13 @@ namespace Email
             services.AddMvc();
             services.AddSingleton(_ => Configuration);
             Services.Config.InjectionConfig.SetupInjections(services);
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +45,11 @@ namespace Email
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+            });
         }
     }
 }
